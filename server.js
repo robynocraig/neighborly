0a173c927b9a253079b586d62e32a4caee5b8eec
mcale017 <mcale017@gmail.com>
2018-07-28 19:01:27 -0500

    trying to get it to run on heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/neighborly");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/neighborly-database");
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
