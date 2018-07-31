d270c3f98febfc4079c1d2c4b6e582b0be99f3a9
mcale017 <mcale017@gmail.com>
2018-07-30 19:28:21 -0500

    authCheck working on 3001

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -8,6 +11,22 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: "https://mcale017.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'https://mcale017-neighborly.herokuapp.com/',
+  issuer: "https://mcale017.auth0.com/",
+  algorithms: ['RS256']
+});
+
+app.use(authCheck);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
