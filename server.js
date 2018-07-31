0f7de0187d1c0c6c68a45527a47ab88ea6791a7a
mcale017 <mcale017@gmail.com>
2018-07-31 15:49:25 -0500

    1st attempt at getting auth0 to work

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,20 +10,6 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
-const authCheck = jwt({
-  secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: "https://mcale017.auth0.com/.well-known/jwks.json"
-  }),
-  audience: 'https://mcale017-neighborly.herokuapp.com/',
-  issuer: "https://mcale017.auth0.com/",
-  algorithms: ['RS256']
-});
-
-app.use(authCheck);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
