a0fdae165bb5bd9a48b349da66b4c1e532675cc1
unknown <stephen.faunillan@gmail.com>
2018-08-01 21:11:50 -0500

    Add nav, post, and profile components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Management from "./pages/Managements";
 import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+import Post from "./components/Post";
 
 const App = () => (
   <Router>
