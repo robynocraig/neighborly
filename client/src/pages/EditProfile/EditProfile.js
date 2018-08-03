2e6b30fb84ab0bdab2cd34d2183d3f43cf67979f
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-08-02 19:52:13 -0500

    adding edit profile to pages

---
+++ client/src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Input, FormBtn } from "../../components/Form";
+// import API from "../../utils/API";
+// import { Link } from "react-router-dom";
+
+class UserProfile extends Component {
+    state = {
+      user: [],
+      name: "",
+      picture: "",
+      residency: ""
+    };
+
+}
