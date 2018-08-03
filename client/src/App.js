70d009b54eebeffed59d24d833eb2b4e0bad8390
Robyn Craig <robynocraig@gmail.com>
2018-08-03 16:09:35 -0500

    added page/component for main/splash page

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Management from "./pages/Managements";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
 import Post from "./components/Post";
+import Splash from "./pages/Splash";
 
 const App = () => (
   <Router>
     <div>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Management} />
+        <Route exact path="/" component={Splash} />
         <Route exact path="/managements" component={Management} />
       </Switch>
     </div>
