1515c33c2c09089a8397fa0912fd627e5fcac93c
Robyn Craig <robynocraig@gmail.com>
2018-08-06 20:55:40 -0500

    added card for homepage sign up modal

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "../../components/Hero";
+import Card from "../../components/Card";
 
 
 const Home = () => (
   <div>
+    <Card />
     <Hero backgroundImage="https://i.imgur.com/6MwReSQ.jpg">
     </Hero>
   </div>
