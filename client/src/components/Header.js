a5dfe1e1e1a491f8bd3165c41e12455134cde555
mcale017 <mcale017@gmail.com>
2018-08-07 21:52:58 -0500

    auth0-lock attempt

---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import isAuthenticated from '../auth/isAuthenticated';
+
+const Header = () => (
+  <header>
+    <h1>React Auth0 App</h1>
+    <nav>
+      <ul>
+        <li><Link to='/'>Top</Link></li>
+        <li><Link to='/private'>Private</Link></li>
+        {
+          !isAuthenticated() && (
+            <li><Link to='/login'>Login</Link></li>
+          )
+        }
+        {
+          isAuthenticated() && (
+            <li><Link to='/logout'>Logout</Link></li>
+          )
+        }
+      </ul>
+    </nav>
+  </header>
+)
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+render((
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('root'));
