0664ec384613bdc2cfeef7ae0fa458d28430b613
unknown <stephen.faunillan@gmail.com>
2018-08-13 19:21:13 -0500

    Working on posting, Brought back tenant page, Able to get post response and post in mongo.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './auth/Login';
 import Logout from './auth/Logout';
 import Testing from './pages/Testing';
 import EditProfile from './pages/EditProfile';
+import Tenant from '../src/pages/Tenant';
 
 const App = () => (
   <div>
@@ -16,6 +17,7 @@ const App = () => (
         <Route exact path="/editprofile" component={EditProfile} />
         {/* <Route path="/private" component={Private} /> */}
         <Route exact path="/Test" component={Testing} />
+        <Route exact path="/tenant" component={Tenant} />
         <Route path="/login" component={Login} />
         <Route path="/logout" component={Logout} />
       </Switch>
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     User: require("./user"),
-    Comments: require("./user")
+    Comments: require("./comments")
 };
