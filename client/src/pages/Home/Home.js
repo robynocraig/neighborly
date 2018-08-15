02305842293aa39b9436043f4bfcb7734dd78dbe
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-08-14 20:18:54 -0500

    adding tenant to home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,6 +3,20 @@ import { Redirect } from 'react-router-dom';
 import isAuthenticated from '../../auth/isAuthenticated';
 import './Home.css';
 import API from '../../utils/API';
+import { Col, Row, Container } from "../../components/Grid";
+import Profile from "../../components/Profile";
+import Post from "../../components/Post";
+import Subnav from "../../components/Subnav";
+
+const styles = {
+    body: {
+      background: "#FCECD6",
+      color: "black",
+      font:" Arial, Helvetica, sans-serif"
+    }
+  };
+
+
 
 class Home extends Component {
     constructor(props) {
@@ -32,6 +46,29 @@ class Home extends Component {
         }
     }
 
+    componentDidMount() {
+        this.loadProfile();
+    }
+
+
+    loadProfile = () => {
+        API.getUsers()
+            .then(res =>
+                this.setState({
+                    users: res.data,
+                    picture: "",
+                    name: "",
+                    address: "",
+                    city: "",
+                    zip: "",
+                    state:"",
+                    about: ""
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
+
     checkUser = email => {
         API.getUser(email)
             .then(res => this.setState({ exists: res.data.exists }))
@@ -53,9 +90,39 @@ class Home extends Component {
                     <div>
                         {
                             (this.state.exists) &&
-                            <div>
-                                I'M PRESENT
-                            </div>
+                            <div style={styles.body}>
+
+<Container>
+  <Row>
+    <Col size="md-3 sm-12">
+    {this.state.users.filter(user => {
+                      console.log(user._id);
+                      return (user.email ==="mbarbotiko@gmail.com")
+
+                      // console.log(user._id);
+                      // return (user.email ===localStorage.email)
+
+                  }).map(user => (
+                      <Profile
+                          picture={user.picture}
+                          name={user.name}
+                          about={user.about}
+                          address={user.address}
+                          city={user.city}
+                          state={user.state}
+                          zip={user.zip} />
+                  ))}
+
+
+    </Col>
+    <Col size="md-9 sm-12">
+      <Subnav />
+      <Post />
+    </Col>
+  </Row>
+</Container>
+
+</div>
                         }
                         {
                             (this.state.exists === false) &&
